a009e87a92addec78b8c383738325dc5f62f7c72
Seong Cheol Park <parksc0711@gmail.com>
2024-06-09 16:04:32 +0900

    - JPA 설정, 멀티모듈 재설정

--- api/src/main/java/com/noogoodd/api/user/adapter/out/mysql/UserRepositoryImpl.java
+++ api/src/main/java/com/noogoodd/api/user/adapter/out/mysql/UserRepositoryImpl.java
@@ -1,4 +1,31 @@
 package com.noogoodd.api.user.adapter.out.mysql;
 
-public class UserRepositoryImpl {
+import com.noogoodd.api.user.adapter.out.jpa.JpaUserRepository;
+import com.noogoodd.api.user.application.port.out.UserRepository;
+import com.noogoodd.common.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final JpaUserRepository jpaUserRepository;
+
+    @Override
+    public UserEntity save(UserEntity user) {
+        return jpaUserRepository.save(user);
+    }
+
+    @Override
+    public Optional<UserEntity> findById(Long id) {
+        return jpaUserRepository.findById(id);
+    }
+
+    @Override
+    public Optional<UserEntity> findByUuid(String uuid) {
+        return jpaUserRepository.findByUuid(uuid);
+    }
 }
--- api/src/main/java/com/noogoodd/api/user/application/port/out/UserRepository.java
+++ api/src/main/java/com/noogoodd/api/user/application/port/out/UserRepository.java
@@ -1,12 +1,13 @@
 package com.noogoodd.api.user.application.port.out;
 
 import com.noogoodd.common.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository {
+    UserEntity save(UserEntity user);
+    Optional<UserEntity> findById(Long id);
     Optional<UserEntity> findByUuid(String uuid);
 }
